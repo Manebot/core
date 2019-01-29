247aefa06cc37e1382a46142f350f4e706a7807c
Manevolent <teamlixo@gmail.com>
2019-01-29 15:08:07 -0500

    Compilation erros

--- src/main/java/com/github/manevolent/jbot/artifact/RemoteArtifact.java
+++ src/main/java/com/github/manevolent/jbot/artifact/RemoteArtifact.java
@@ -1,12 +1,8 @@
 package com.github.manevolent.jbot.artifact;
 
-import com.github.manevolent.jbot.plugin.java.JavaPlugin;
-
-import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Collection;
 
 public interface RemoteArtifact extends Artifact {
 
@@ -23,14 +19,6 @@ public interface RemoteArtifact extends Artifact {
         } catch (URISyntaxException e) {
             throw new IllegalArgumentException(e);
         }
-    }
-
-    /**
-     * Loads this artifact as a plugin, if it has not been loaded already.
-     * @return JavaPlugin instance of this plugin.
-     */
-    default JavaPlugin loadAsPlugin() {
-        throw new UnsupportedOperationException("cannot load remote artifact; use obtain() to obtain an artifact");
     }
 
 }
