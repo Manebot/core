953ef8877a62f998412666aad3ba705e17d4fdec
Manevolent <teamlixo@gmail.com>
2019-02-01 09:02:47 -0500

    Dependency level fixes

--- src/main/java/com/github/manevolent/jbot/artifact/ArtifactDependencyLevel.java
+++ src/main/java/com/github/manevolent/jbot/artifact/ArtifactDependencyLevel.java
@@ -3,15 +3,24 @@ package com.github.manevolent.jbot.artifact;
 public enum ArtifactDependencyLevel {
 
     /**
-     * Indicates an artifact dependency is only required at compile-time, and should not be included at run-time.
-     *
-     * These are often required for plugin co-dependencies.
+     * Indicates an artifact dependency is required at compile-time, and should be packaged with an artifact locally.
      */
     COMPILE,
 
     /**
-     * Indicates an artifact dependency is required both at compile and run-time.
+     * Indicates an artifact dependency is required at run-time.
      */
-    RUN
+    RUN,
+
+    /**
+     * Indicates a dependency is required to be provided by the JRE.
+     * The bot treats these as shared libraries and/or plugins.
+     */
+    PROVIDED,
+
+    /**
+     * Indicates a test dependency, only used for testing at run-time.
+     */
+    TEST
 
 }
