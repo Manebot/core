a45f3fe11cda529562aa699359bd358cc08ff2e6
Manevolent <teamlixo@gmail.com>
2019-03-19 19:10:28 -0400

    EventDispatcher needs async support

--- src/main/java/com/github/manevolent/jbot/event/EventDispatcher.java
+++ src/main/java/com/github/manevolent/jbot/event/EventDispatcher.java
@@ -1,12 +1,23 @@
 package com.github.manevolent.jbot.event;
 
+import java.util.concurrent.Future;
+
 public interface EventDispatcher {
 
     /**
-     * Fires an event
+     * Fires an event.
      * @param event Event to fire.
      * @return Event
+     * @throws EventExecutionException if an exception occurred executing the event.
      */
-    Event fire(Event event) throws EventExecutionException;
+    Event execute(Event event) throws EventExecutionException;
+
+    /**
+     * Fires an event asynchronously.
+     * @param event Event to fire.
+     * @param <T> Event type.
+     * @return Event future.
+     */
+    <T extends Event> Future<T> executeAsync(T event);
 
 }
