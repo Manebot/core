e3536f98b83f4bf51ce5ad27a06e661acca43dd8
Manevolent <teamlixo@gmail.com>
2019-03-23 17:44:56 -0400

    matchMode.toMatchString() needed from hibernate

---
+++ src/main/java/com/github/manevolent/jbot/database/expressions/ExtendedExpressions.java
@@ -0,0 +1,24 @@
+package com.github.manevolent.jbot.database.expressions;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
+
+public final class ExtendedExpressions {
+    public static Predicate escapedLike(CriteriaBuilder criteriaBuilder, Expression<String> left, String right,
+                                        MatchMode matchMode) {
+        return criteriaBuilder.like(left, matchMode.toMatchString(escapeLike(right)), '!');
+    }
+
+    public static Predicate escapedNotLike(CriteriaBuilder criteriaBuilder, Expression<String> left, String right,
+                                           MatchMode matchMode) {
+        return criteriaBuilder.notLike(left, matchMode.toMatchString(escapeLike(right)), '!');
+    }
+
+    private static String escapeLike(String value) {
+        return value
+                .replace("!", "!!")
+                .replace("%", "!%")
+                .replace("_", "!_");
+    }
+}
