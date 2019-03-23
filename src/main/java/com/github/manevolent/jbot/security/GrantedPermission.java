d5050ffc8675ec152b0f0d1c44368313e1c90f95
Manevolent <teamlixo@gmail.com>
2019-03-22 20:37:34 -0400

    CommandAccessException was not fully typed out

--- src/main/java/com/github/manevolent/jbot/security/GrantedPermission.java
+++ src/main/java/com/github/manevolent/jbot/security/GrantedPermission.java
@@ -7,14 +7,40 @@ import java.util.Date;
 
 public interface GrantedPermission {
 
+    /**
+     * Gets the entity that this granted permission is associated with.
+     * @return Entity instance.
+     */
     Entity getEntity();
 
+    /**
+     * Gets the permission node that this granted permission refers to.
+     * @return
+     */
     Permission getPermission();
 
+    /**
+     * Gets the grant that is explicitly defined by this permission.
+     * @return Grant instance.
+     */
     Grant getGrant();
 
+    /**
+     * Gets the user that granted this permission.
+     * @return User instance.
+     */
     User getGranter();
 
+    /**
+     * Gets the date this permission was granted.
+     * @return Date instance.
+     */
     Date getDate();
+
+    /**
+     * Removes this granted permission
+     * @throws SecurityException ifa security violation is detected attempting to remove this permission.
+     */
+    void remove() throws SecurityException;
 
 }
