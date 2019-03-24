9ea250083bde204302758013984d6371cb67947b
Manevolent <teamlixo@gmail.com>
2019-03-24 04:10:25 -0400

    Plugin Future api

--- src/main/java/com/github/manevolent/jbot/plugin/PluginReference.java
+++ src/main/java/com/github/manevolent/jbot/plugin/PluginReference.java
@@ -2,8 +2,8 @@ package com.github.manevolent.jbot.plugin;
 
 public interface PluginReference {
 
-    void load(Plugin plugin);
+    void load(Plugin.Future plugin);
 
-    void unload(Plugin plugin);
+    void unload(Plugin.Future plugin);
 
 }
