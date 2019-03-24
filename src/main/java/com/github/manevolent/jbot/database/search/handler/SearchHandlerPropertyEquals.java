8b90bb4631ac3a31309f0600603c7ea170be19cf
Manevolent <teamlixo@gmail.com>
2019-03-23 21:14:59 -0400

    Type conversion

--- src/main/java/com/github/manevolent/jbot/database/search/handler/SearchHandlerPropertyEquals.java
+++ src/main/java/com/github/manevolent/jbot/database/search/handler/SearchHandlerPropertyEquals.java
@@ -9,16 +9,30 @@ import javax.persistence.criteria.Root;
 import java.util.function.Function;
 
 public class SearchHandlerPropertyEquals extends SearchHandlerEntityProperty {
+    private final Function<String, ?> parser;
+
     public SearchHandlerPropertyEquals(Function<Root, Path> pathFinder) {
-        super(pathFinder);
+        this(pathFinder, String::toString);
     }
 
     public SearchHandlerPropertyEquals(String property) {
+        this(property, String::toString);
+    }
+
+    public SearchHandlerPropertyEquals(Function<Root, Path> pathFinder, Function<String, ?> parser) {
+        super(pathFinder);
+
+        this.parser = parser;
+    }
+
+    public SearchHandlerPropertyEquals(String property, Function<String, ?> parser) {
         super(property);
+
+        this.parser = parser;
     }
 
     @Override
     protected Predicate handle(Path path, CriteriaBuilder criteriaBuilder, SearchArgument value) {
-        return criteriaBuilder.equal(path, value.getValue());
+        return criteriaBuilder.equal(path, parser.apply(value.getValue()));
     }
 }
