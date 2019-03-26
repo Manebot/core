a4ddedb9d43d298689cc38b187d0c4ff0e115485
Manevolent <teamlixo@gmail.com>
2019-03-26 15:16:42 -0400

    Version

--- src/main/java/com/github/manevolent/jbot/Version.java
+++ src/main/java/com/github/manevolent/jbot/Version.java
@@ -1,5 +1,8 @@
 package com.github.manevolent.jbot;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 /**
  * Describes a jBot version.
  *
@@ -24,89 +27,34 @@ package com.github.manevolent.jbot;
  *  1.5.15-abcdefg1234 to include buildId
  */
 public class Version implements Comparable<Version> {
-    private final int major, minor, micro;
+    private final int[] components;
     private final String buildId;
 
-    public Version(int major, int minor, int micro, String buildId) {
-        this.major = major;
-        this.minor = minor;
-        this.micro = micro;
+    public Version(int[] components, String buildId) {
+        this.components = components;
         this.buildId = buildId;
     }
 
-    /**
-     * Gets this version's major version.
-     *
-     * major.minor.macro-buildId
-     *
-     * @return Major version.
-     */
-    public int getMajor() {
-        return major;
-    }
-
-
-    /**
-     * Gets this plugin's minor version.
-     *
-     * major.minor.macro-buildId
-     *
-     * @return Minor version.
-     */
-    public int getMinor() {
-        return minor;
-    }
-
-    /**
-     * Gets this plugin's micro version.
-     *
-     * major.minor.macro-buildId
-     *
-     * @return Micro version.
-     */
-    public int getMicro() {
-        return micro;
-    }
-
-    /**
-     * Gets this version's build ID.
-     *
-     * major.minor.macro-buildId
-     *
-     * Note that "buildId" is always ignored in comparison, is ALWAYS compared in equality, and should be considered
-     * only when testing by a developer or to match JAR/Git commit versions on a plugin-by-plugin basis.
-     *
-     * @return build Id.
-     */
-    public String getBuildId() {
-        return buildId;
+    public int getComponent(int index) {
+        if (components.length < index) return 0;
+        else return components[index];
     }
 
     @Override
     public int compareTo(Version o) {
-        int majorCompare = Integer.compare(getMajor(), o.getMajor());
-        if (majorCompare != 0) {
-            return majorCompare;
-        } else {
-            int minorCompare = Integer.compare(getMinor(), o.getMinor());
-            if (minorCompare != 0) {
-                return minorCompare;
-            } else {
-                int microCompare = Integer.compare(getMicro(), o.getMicro());
-                if (microCompare != 0) {
-                    return microCompare;
-                } else {
-                    // buildId is not compared
-                    return 0;
-                }
-            }
+        int len = Math.max(o.components.length, components.length);
+        for (int componentIndex = 0; componentIndex < len; componentIndex ++) {
+            int c = Integer.compare(getComponent(componentIndex), o.getComponent(componentIndex));
+            if (c != 0) return c;
         }
+
+        return 0;
     }
 
     @Override
     public String toString() {
-        return Integer.toString(major) + "." + Integer.toString(minor) + "." + Integer.toString(micro) +
-                (buildId != null ? ("-" + buildId) : "");
+        return String.join(".", Arrays.stream(components).mapToObj(String::valueOf).collect(Collectors.toList()))
+                + (buildId != null ? ("-" + buildId) : "");
     }
 
     @Override
@@ -115,44 +63,15 @@ public class Version implements Comparable<Version> {
     }
 
     public boolean equals(Version b) {
-        return b != null &&
-                major == b.major &&
-                minor == b.minor &&
-                micro == b.micro &&
-                buildId.equals(b.buildId);
+        return b != null && Arrays.equals(components, b.components) && buildId.equals(b.buildId);
     }
 
     public static Version fromString(String versionString) {
         String[] parts = versionString.split("\\-", 2);
-        String artifactId = parts.length == 2 ? parts[1] : null;
-
-        String[] numericParts = parts[0].split("\\.", 3);
-
-        switch (numericParts.length) {
-            case 1:
-                return new Version(
-                        Integer.parseInt(numericParts[0]),
-                        0,
-                        0,
-                        artifactId
-                );
-            case 2:
-                return new Version(
-                        Integer.parseInt(numericParts[0]),
-                        Integer.parseInt(numericParts[1]),
-                        0,
-                        artifactId
-                );
-            case 3:
-                return new Version(
-                        Integer.parseInt(numericParts[0]),
-                        Integer.parseInt(numericParts[1]),
-                        Integer.parseInt(numericParts[2]),
-                        artifactId
-                );
-            default:
-                throw new IllegalArgumentException("invalid version string");
-        }
+        String buildId = parts.length == 2 ? parts[1] : null;
+        String[] componentStrings = parts[0].split("\\.");
+        int[] components = Arrays.stream(componentStrings).mapToInt(Integer::parseInt).toArray();
+        return new Version(components, buildId);
     }
 
 }
